51275108c01ce6f85c4c94cd0d2bd598d01d1f0d
Vadim Akhmatov <vadim.akh42@gmail.com>
2022-12-08 03:26:08 +0700

    Update UI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,28 @@
 ﻿using Application;
-using Core.Enums;
 
 var cb = new ChordsBuiler();
-var chord = cb.GetChord(Note.GSharp);
 
-Console.ReadKey();
+while (true)
+{
+    Console.Write("Enter a chord: ");
+    var chordText = Console.ReadLine();
+
+    try
+    {
+        var chord = cb.ParseChord(chordText);
+
+        Console.WriteLine(chord);
+    }
+    catch (FormatException ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: {ex.Message}");
+        Console.ResetColor();
+    }
+    finally
+    {
+        Console.WriteLine("Press enter to continue...");
+
+        Console.ReadKey(true);
+    }
+}
