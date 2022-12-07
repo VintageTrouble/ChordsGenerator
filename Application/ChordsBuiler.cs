cb6882105320afba6e6cec9e533f26f7e20321d8
Vadim Akhmatov <vadim.akh42@gmail.com>
2022-12-08 03:25:12 +0700

    Update chord builder

--- Application/ChordsBuiler.cs
+++ Application/ChordsBuiler.cs
@@ -5,8 +5,77 @@ namespace Application;
 
 public class ChordsBuiler
 {
-    public Chord GetChord(Note note)
+    private readonly char[] _noteSigns = new char[] { '#', 'b' };
+    public Chord GetChord(Note note, Tone tone)
     {
-        return new Chord(note.ToString(), note);
+        return new Chord(note.ToString(), note, tone);
+    }
+
+    //D
+    //Db
+    //D#
+    //Dbm
+    //D#m
+    //Em
+    //!E#m
+    //Ebm
+    public Chord ParseChord(string? chord)
+    {
+        if (string.IsNullOrWhiteSpace(chord) || chord.Length > 3)
+            throw new FormatException("Invalid format of a chord.");
+
+        if (!Enum.GetNames(typeof(Note)).Contains(chord[0].ToString()))
+            throw new FormatException($"Can't find tonic {chord[0]} for chord.");
+
+        return chord.Length == 1
+            ? new Chord(chord, Enum.Parse<Note>(chord), Tone.Major)
+            : ParseModifiedChord(chord);
+    }
+
+    private Chord ParseModifiedChord(string chord)
+    {
+        var hasSharp = !(chord[0] == 'E' || chord[0] == 'B');
+        var maxLength = hasSharp ? 3 : 2;
+        var sign = chord[chord.Length - (chord.Length == 3 ? 2 : 1)];
+
+        if (!hasSharp && sign == _noteSigns[0])
+            throw new FormatException("Sharp for E and B chords not exists.");
+
+        var tone = ParseTone(chord, maxLength);
+
+        if(chord.Length == 2)
+            return new Chord(chord, Enum.Parse<Note>(chord[0].ToString()), tone);
+
+        switch (sign)
+        {
+            case '#':
+                return new Chord(chord, Enum.Parse<Note>(chord[0] + "Sharp"), tone);
+
+            case 'b':
+                var tonic = Enum.Parse<Note>(chord[0] + (hasSharp ? "Sharp" : "")) - 1;
+                tonic = tonic < 0 ? (int)Note.B - tonic : tonic;
+
+                return new Chord(chord, tonic, tone);
+
+            default:
+                throw new FormatException("Undeclared key sign.");
+        }
+    }
+
+    private Tone ParseTone(string chord, int maxLength)
+    {
+        if (chord.Length == maxLength)
+        {
+            var sign = chord[maxLength - 1];
+            if (sign == 'm')
+                return Tone.Minor;
+
+            if (_noteSigns.Contains(sign))
+                return Tone.Major;
+
+            throw new FormatException("Can't detect note sign.");
+        }
+
+        return Tone.Major;
     }
 }
--- Core/Entities/Chord.cs
+++ Core/Entities/Chord.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 using Core.Enums;
 using Core.Extensions;
 
@@ -7,29 +9,37 @@ namespace Core.Entities;
 public class Chord
 {
     public string Name { get; set; }
-    public IEnumerable<Note> Notes { get; set; }
+    public List<Note> Notes { get; set; } = null!;
 
     private Chord(string name)
     {
         Name = name;
     }
 
-    public Chord(string name, IEnumerable<Note> notes)
-        : this(name)
-    {
-        Notes = notes;
-    }
-
-    public Chord(string name, Note tonic)
+    public Chord(string name, Note tonic, Tone tone)
         : this(name)
     {
         var notes = new List<Note>
         {
             tonic,
-            tonic.AddInterval(Interval.M3),
+            tonic.AddInterval(tone == Tone.Major ? Interval.M3 : Interval.m3),
             tonic.AddInterval(Interval.M3, Interval.m3)
         };
 
         Notes = notes;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"Chord: {Name}. Notes: ");
+
+        foreach (var item in Notes)
+        {
+            sb.Append($"{item.ToString().Replace("Sharp", "#")}, ");
+        }
+        
+
+        return sb.ToString().Trim(", ".ToArray());
     }
 }
